4a37690b570f2cd4fbc3289d2ba32c42a3e4628c
RJ <r.j.kobish@gmail.com>
2019-07-07 02:16:26 -0400

    FOV highlighting without fog of war

--- render_functions.py
+++ render_functions.py
@@ -21,7 +21,7 @@ def render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, sc
 
     # Draw all entities in the list
     for entity in entities:
-        draw_entity(con, entity)
+        draw_entity(con, entity, fov_map)
 
     libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
 
@@ -29,9 +29,10 @@ def clear_all(con, entities):
     for entity in entities:
         clear_entity(con, entity)
 
-def draw_entity(con, entity):
-    libtcod.console_set_default_foreground(con, entity.color)
-    libtcod.console_put_char(con, entity.x, entity.y, entity.char, libtcod.BKGND_NONE)
+def draw_entity(con, entity, fov_map):
+    if libtcod.map_is_in_fov(fov_map, entity.x, entity.y):
+        libtcod.console_set_default_foreground(con, entity.color)
+        libtcod.console_put_char(con, entity.x, entity.y, entity.char, libtcod.BKGND_NONE)
 
 def clear_entity(con, entity):
     # Erase the character that represents this object
