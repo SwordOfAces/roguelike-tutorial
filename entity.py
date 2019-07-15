4e11f2a57e28825e6c204729488bab905e3b8d04
RJ <r.j.kobish@gmail.com>
2019-07-15 04:27:34 -0400

    vi keys and A* monster movement

--- entity.py
+++ entity.py
@@ -1,5 +1,7 @@
 import math
 
+import tcod as libtcod
+
 class Entity:
     """
     A generic object to represent players, enemies, items, etc.
@@ -36,6 +38,54 @@ class Entity:
         if not (game_map.is_blocked(self.x + dx, self.y + dy) or
                 get_blocking_entities_at_location(entities, self.x + dx, self.y + dy)):
             self.move(dx, dy)
+
+    def move_astar(self, target, entities, game_map):
+        # A* search algorithm lets our monsters move diagonally, smartly
+
+        # Create a FOV map that has the dimensions of the map
+        fov = libtcod.map_new(game_map.width, game_map.height)
+
+        # Scan the current map each turn and set all the walls as unwalkable
+        for y1 in range(game_map.height):
+            for x1 in range(game_map.width):
+                libtcod.map_set_properties(fov, x1, y1, not game_map.tiles[x1][y1].block_sight, not game_map.tiles[x1][y1].blocked)
+
+        # Scan all the objects to see if there are objects that must be
+        # navigated around. Check also that the object isn't self or the
+        # target (so that the start/end points are free)
+        # The AI class handles the situation if the self is next to the
+        # target so it won't use this A* function anyway.
+        for entity in entities:
+            if entity.blocks and entity != self and entity != target:
+                # Set the tile as a 'wall' so it must be navigated around
+                libtcod.map_set_properties(fov, entity.x, entity.y, True, False)
+
+        # Allocate a A* path
+        # The 1.41 is the normal diagonal cost of moving
+        # it can be set as 0.0 to disable diagonal moving
+        path = libtcod.path_new_using_map(fov, 1.41)
+
+        # Check if the path exists, and in this case, also the path is
+        # shorter than 25 tiles. THe path size matters if you want the
+        # monster to use alt longer paths (eg through other rooms) if eg
+        # the player is in a corridor. It makes sense to keep path size
+        # relatively low to keep monsters from running around the map if
+        # there's an alternative path really far away
+        if not libtcod.path_is_empty(path) and libtcod.path_size(path) < 25:
+            # Find the next coordinates in the computer full path
+            x, y = libtcod.path_walk(path, True)
+            if x or y:
+                # Set self's coordinates to the next path tile
+                self.x = x
+                self.y = y
+        else:
+            # Keep the old move function as a backup so that if there are
+            # no paths (eg another monster blocks a corridor) it will still
+            # try to move towards the player (closer to the corridor)
+            self.move_towards(target.x, target.y, game_map, entities)
+
+        # Delete the path to free memory
+        libtcod.path_delete(path)
 
     def distance_to(self, other):
         dx = other.x - self.x
