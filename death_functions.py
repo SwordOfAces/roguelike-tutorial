ec9c262a7b3b95ad3414c160a9c05e144245562d
RJ <r.j.kobish@gmail.com>
2019-07-15 05:11:34 -0400

    render in order of importance

--- death_functions.py
+++ death_functions.py
@@ -1,6 +1,7 @@
 import tcod as libtcod
 
 from game_states import GameStates
+from render_functions import RenderOrder
 
 def kill_player(player):
     player.char = '%'
@@ -8,12 +9,13 @@ def kill_player(player):
     return 'You died!', GameStates.PLAYER_DEAD
 
 def kill_monster(monster):
-    death_message = '{monster.name.capitalize()} is dead!'
+    death_message = f'{monster.name.capitalize()} is dead!'
     monster.char = '%'
     monster.color = libtcod.dark_red
     monster.blocks = False
     monster.fighhter = None
     monster.ai = None
     monster.name = 'remains of ' + monster.name
+    monster.render_order = RenderOrder.CORPSE
 
     return death_message
