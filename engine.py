97670e3f2cc49c35e87ae1fbf22f5b98297feea7
RJ <r.j.kobish@gmail.com>
2019-07-30 00:56:02 -0400

    inventory menu

--- engine.py
+++ engine.py
@@ -70,6 +70,7 @@ def main():
     mouse = libtcod.Mouse()
 
     game_state = GameStates.PLAYERS_TURN
+    previous_game_state = game_state
 
     while not libtcod.console_is_window_closed():
         # Capture key & mouse events
@@ -78,7 +79,7 @@ def main():
         if fov_recompute:
             recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
 
-        render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, message_log, screen_width, screen_height, bar_width, panel_height, panel_y, mouse, colors)
+        render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, message_log, screen_width, screen_height, bar_width, panel_height, panel_y, mouse, colors, game_state)
         
         fov_recompute = False
 
@@ -89,6 +90,7 @@ def main():
 
         move = action.get('move')
         pickup = action.get('pickup')
+        show_inventory = action.get('show_inventory')
         exit = action.get('exit')
         fullscreen = action.get('fullscreen')
 
@@ -116,8 +118,15 @@ def main():
             else:
                 message_log.add_message(Message('There is nothing here to pick up', libtcod.yellow))
 
+        if show_inventory:
+            previous_game_state = game_state
+            game_state = GameStates.SHOW_INVENTORY
+
         if exit:
-            return True
+            if game_state == GameStates.SHOW_INVENTORY:
+                game_state = previous_game_state
+            else:
+                return True
 
         if fullscreen:
             libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
--- render_functions.py
+++ render_functions.py
@@ -1,6 +1,9 @@
-from enum import Enum
-
 import tcod as libtcod
+
+from enum import Enum
+from game_states import GameStates
+from menus import inventory_menu
+
 
 class RenderOrder(Enum):
     CORPSE = 1
@@ -33,7 +36,7 @@ def render_bar(panel, x, y, total_width, name, value, maximum, bar_color, back_c
 
 def render_all(con, panel, entities, player, game_map, fov_map,
     fov_recompute, message_log, screen_width, screen_height, bar_width,
-    panel_height, panel_y, mouse, colors):
+    panel_height, panel_y, mouse, colors, game_state):
 
     # Draw all the tiles in the game_map
     if fov_recompute:
@@ -78,6 +81,9 @@ def render_all(con, panel, entities, player, game_map, fov_map,
     libtcod.console_print_ex(panel, 1, 0, libtcod.BKGND_NONE, libtcod.LEFT, get_names_under_mouse(mouse, entities, fov_map))
 
     libtcod.console_blit(panel, 0, 0, screen_width, panel_height, 0, 0, panel_y)
+
+    if game_state == GameStates.SHOW_INVENTORY:
+        inventory_menu(con, 'Press the key next to an item to use it, or Esc to cancel.\n', player.inventory, 50, screen_width, screen_height)
 
 
 def clear_all(con, entities):
