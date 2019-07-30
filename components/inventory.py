3d9c7a5d77a96ecff8ba80a5543dd9420487e166
RJ <r.j.kobish@gmail.com>
2019-07-30 01:44:08 -0400

    dropping items

--- components/inventory.py
+++ components/inventory.py
@@ -48,3 +48,15 @@ class Inventory:
 
     def remove_item(self, item):
         self.items.remove(item)
+
+
+    def drop_item(self, item):
+        results = []
+
+        item.x = self.owner.x
+        item.y = self.owner.y
+
+        self.remove_item(item)
+        results.append({'item_dropped': item, 'message': Message(f'You dropped the {item.name}', libtcod.yellow)})
+
+        return results
--- engine.py
+++ engine.py
@@ -91,6 +91,7 @@ def main():
         move = action.get('move')
         pickup = action.get('pickup')
         show_inventory = action.get('show_inventory')
+        drop_inventory = action.get('drop_inventory')
         inventory_index = action.get('inventory_index')
         exit = action.get('exit')
         fullscreen = action.get('fullscreen')
@@ -123,12 +124,20 @@ def main():
             previous_game_state = game_state
             game_state = GameStates.SHOW_INVENTORY
 
+        if drop_inventory:
+            previous_game_state = game_state
+            game_state = GameStates.DROP_INVENTORY
+
         if inventory_index is not None and previous_game_state != GameStates.PLAYER_DEAD and inventory_index < len(player.inventory.items):
             item = player.inventory.items[inventory_index]
-            player_turn_results.extend(player.inventory.use(item))
+
+            if game_state == GameStates.SHOW_INVENTORY:
+                player_turn_results.extend(player.inventory.use(item))
+            elif game_state == GameStates.DROP_INVENTORY:
+                player_turn_results.extend(player.inventory.drop_item(item))
 
         if exit:
-            if game_state == GameStates.SHOW_INVENTORY:
+            if game_state in (GameStates.SHOW_INVENTORY, GameStates.DROP_INVENTORY):
                 game_state = previous_game_state
             else:
                 return True
@@ -141,6 +150,7 @@ def main():
             dead_entity = result.get('dead')
             item_added = result.get('item_added')
             item_consumed = result.get('consumed')
+            item_dropped = result.get('item_dropped')
 
             if message:
                 message_log.add_message(message)
@@ -159,6 +169,11 @@ def main():
 
             if item_consumed:
                 game_state = GameStates.ENEMY_TURN
+
+            if item_dropped:
+                entities.append(item_dropped)
+                game_state = GameStates.ENEMY_TURN
+                # Dropping an item takes your turn? TODO
 
         if game_state == GameStates.ENEMY_TURN:
             for entity in entities:
