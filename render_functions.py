68991f7aeead619b4fda24b1399c64a6901cf9cb
RJ <r.j.kobish@gmail.com>
2019-08-01 19:19:06 -0400

    stairs lead to deeper levels

--- render_functions.py
+++ render_functions.py
@@ -76,7 +76,10 @@ def render_all(con, panel, entities, player, game_map, fov_map,
         libtcod.console_print_ex(panel, message_log.x, y, libtcod.BKGND_NONE, libtcod.LEFT, message.text)
         y += 1
 
-    render_bar(panel, 1, 1, bar_width, 'HP', player.fighter.hp, player.fighter.max_hp, libtcod.light_red, libtcod.darker_red)
+    render_bar(panel, 1, 1, bar_width, 'HP', player.fighter.hp,
+            player.fighter.max_hp, libtcod.light_red, libtcod.darker_red)
+    libtcod.console_print_ex(panel, 1, 3, libtcod.BKGND_NONE, libtcod.LEFT,
+            f'Dungeon level: {game_map.dungeon_level}'))
 
     libtcod.console_set_default_foreground(panel, libtcod.light_gray)
     libtcod.console_print_ex(panel, 1, 0, libtcod.BKGND_NONE, libtcod.LEFT, get_names_under_mouse(mouse, entities, fov_map))
