2b39ddd9bb73d3d4171d3c7876e88da00f075e36
RJ <r.j.kobish@gmail.com>
2019-08-01 23:44:57 -0400

    difficulty curve

--- loader_functions/initialize_new_game.py
+++ loader_functions/initialize_new_game.py
@@ -49,8 +49,6 @@ def get_constants():
             'fov_light_walls': True, # brighten the walls we can see,
             'fov_radius': 10, # how far can we see?,
 
-            'max_monsters_per_room': 3,
-            'max_items_per_room': 6,
             'colors': colors
         }
 
@@ -59,7 +57,7 @@ def get_constants():
 
 def get_game_variables(constants):
     # Set up player:
-    fighter_component = Fighter(hp=30, defense=2, power=5)
+    fighter_component = Fighter(hp=100, defense=1, power=4)
     inventory_component = Inventory(26)
     level_component = Level()
     player = Entity(0, 0, '@', libtcod.white, 'Player', blocks=True,
@@ -69,7 +67,7 @@ def get_game_variables(constants):
 
     # Set up game map:
     game_map = GameMap(constants['map_width'], constants['map_height'])
-    game_map.make_map(constants['max_rooms'], constants['room_min_size'], constants['room_max_size'], constants['map_width'], constants['map_height'], player, entities, constants['max_monsters_per_room'], constants['max_items_per_room'])
+    game_map.make_map(constants['max_rooms'], constants['room_min_size'], constants['room_max_size'], constants['map_width'], constants['map_height'], player, entities)
 
     message_log = MessageLog(constants['message_x'], constants['message_width'], constants['message_height'])
 
