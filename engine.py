2ee72eabf4fc6698661c77b963b63ade40411f45
RJ <r.j.kobish@gmail.com>
2019-08-02 15:12:36 -0400

    equippable item setup

--- engine.py
+++ engine.py
@@ -114,12 +114,12 @@ def play_game(player, entities, game_map, message_log, game_state, con, panel, c
 
         if level_up:
             if level_up == 'hp':
-                player.fighter.max_hp += 20
+                player.fighter.base_max_hp += 20
                 player.fighter.hp += 20
             elif level_up == 'str':
-                player.fighter.power += 1
+                player.fighter.base_power += 1
             elif level_up == 'def':
-                player.fighter.defense += 1
+                player.fighter.base_defense += 1
 
             game_state = previous_game_state
 
@@ -154,6 +154,7 @@ def play_game(player, entities, game_map, message_log, game_state, con, panel, c
             item_added = result.get('item_added')
             item_consumed = result.get('consumed')
             item_dropped = result.get('item_dropped')
+            equip = result.get('equip')
             targeting = result.get('targeting')
             targeting_cancelled = result.get('targeting_cancelled')
             xp = result.get('xp')
@@ -203,6 +204,22 @@ def play_game(player, entities, game_map, message_log, game_state, con, panel, c
                 entities.append(item_dropped)
                 game_state = GameStates.ENEMY_TURN
                 # Dropping an item takes your turn? TODO
+
+            if equip:
+                equip_results = player.equipment.toggle_equip(equip)
+
+                for equip_result in equip_results:
+                    equipped = equip_result.get('equipped')
+                    dequipped = equip_result.get('dequipped')
+
+                    if equipped:
+                        message_log.add_message(
+                                Message(f'You equipped the {equipped.name}'))
+                    if dequipped:
+                        message_log.add_message(
+                                Message(f'You dequipped the {equipped.name}'))
+
+                game_state = GameStates.ENEMY_TURN
 
         if game_state == GameStates.ENEMY_TURN:
             for entity in entities:
